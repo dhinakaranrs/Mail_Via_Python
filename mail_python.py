9d902b4ce4fcaa81c8576937240722b834615497
Dhinakaran RS <50867934+dhinakaranrs@users.noreply.github.com>
2021-07-03 13:58:52 +0530

    Add files via upload

---
+++ mail_python.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.message import EmailMessage
+
+msg=EmailMessage()
+msg['subject']='something'
+msg['From']='USER'
+msg['To']='mention mail'
+msg.set_content("Test Email")
+
+with open('output.txt') as myfile:
+    data=myfile.read()
+    msg.set_content(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+server.login("mention mail server credentials"," ")
+server.send_message(msg)
+server.quit()
