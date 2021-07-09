6aaac7d77eddd96449d4b24387cc42a25285a3f8
Dhinakaran RS <50867934+dhinakaranrs@users.noreply.github.com>
2021-07-09 14:47:14 +0530

    Update mail_python.py

--- mail_python.py
+++ mail_python.py
@@ -11,18 +11,10 @@ with open('output.txt') as myfile:
     data=myfile.read()
     msg.set_content(data)
 
+''''
+write something here
 
-
-
-
-
-
-
-
-
-
-
-
+''''
 server=smtplib.SMTP_SSL('smtp.gmail.com',465)
 server.login("mention mail server credentials"," ")
 server.send_message(msg)
